81c9e2d0fa408b326efca5080f4f07e2927e4fed
Joy Bose <joy.bose@cimet.com.au>
2024-12-12 17:28:05 +0530

    products route created

--- backend/server.ts
+++ backend/server.ts
@@ -7,11 +7,29 @@ dotenv.config({ path: "./backend/.env" });
 const app = express();
 app.use(express.json());
 
+// root
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is ready");
 });
 
-app.post("/api/products", async (req: Request, res: Response) => {
+// get all products
+app.get("/api/products", async (req: Request, res: Response) => {
+  try {
+    const allProducts = await Product.find({});
+    return res.status(200).json({
+      success: true,
+      message: "all products fetched successfully",
+      data: allProducts,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: `server error ${error}` });
+  }
+});
+
+// add new product
+app.post("/api/product", async (req: Request, res: Response) => {
   const product: IProduct = req.body;
 
   if (!product.name || !product.image || !product.price) {
@@ -27,11 +45,69 @@ app.post("/api/products", async (req: Request, res: Response) => {
       .status(201)
       .json({ success: true, message: "product added successfully" });
   } catch (error: any) {
-    console.log("Error in creating product:", error);
-    res.status(500).json({ success: false, message: error });
+    res
+      .status(500)
+      .json({ success: false, message: `Error in creating product. ${error}` });
   }
 });
 
+// update existing product
+app.put("/api/product/:id", async (req: Request, res: Response) => {
+  const currentId = req.params.id;
+  if (!currentId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "invalid valid id" });
+  }
+  const existingProduct = req.body;
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      currentId,
+      existingProduct,
+      { new: true }
+    );
+    return res.status(200).json({
+      success: true,
+      message: `product updated successfully`,
+      data: updatedProduct,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: `Error in updating product. ${error}` });
+  }
+});
+
+// delete existing product
+app.delete("/api/product/:id", async (req: Request, res: Response) => {
+  const currentId = req.params.id;
+  if (!currentId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "invalid valid ID" });
+  }
+  try {
+    const currentProduct = await Product.findById(currentId);
+    if (!currentProduct) {
+      return res.status(404).json({
+        success: false,
+        message: `product with ID: ${currentId} does not exists`,
+      });
+    } else {
+      await Product.findByIdAndDelete(currentId);
+      return res.status(201).json({
+        success: true,
+        message: `product with ID: ${currentId} deleted successfully`,
+      });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: `Error in creating product. ${error}` });
+  }
+});
+
+// server
 app.listen(5000, () => {
   connectDB();
   console.log("server started at http://localhost:5000");
