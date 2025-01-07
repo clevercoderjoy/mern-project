a89afe965cd241cfdbd85405a3212aa3b6eee475
clevercoderjoy <clevercoderjoy@gmail.com>
2025-01-07 19:41:35 +0530

    build fix

--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,9 @@ import cors from "cors";
 dotenv.config({ path: "./.env" });
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({ origin: "https://clevercoderjoy-first-mern-project.netlify.app/" })
+);
 
 // root
 app.get("/", (req: Request, res: Response) => {
