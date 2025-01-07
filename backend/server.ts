993e48e8359e8d0a84ffed85518544f778d9929b
clevercoderjoy <clevercoderjoy@gmail.com>
2025-01-07 23:25:25 +0530

    build fix

--- backend/server.ts
+++ backend/server.ts
@@ -7,17 +7,29 @@ import cors from "cors";
 dotenv.config({ path: "./.env" });
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // root
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is ready");
 });
 
+app.get("/.well-known/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "OK" });
+});
+
 app.use("/api/products", productRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 // server
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log("server started at https://mern-project-asso.onrender.com");
 });
